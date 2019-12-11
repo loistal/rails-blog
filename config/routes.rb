b728514a1dfe470e4353c2e855f337473cd9b64b
Lois Talagrand <lois.talagrand@gmail.com>
2019-12-10 18:16:07 -1000

    Redirect root to articles index and style homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :articles
+  resources :users
+
+  root 'articles#index'
 
   get "signup", to: 'users#new'
   post "users", to: 'users#create'
