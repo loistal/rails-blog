3382b42226de057f873d956ca441d37607349149
Lois Talagrand <lois.talagrand@gmail.com>
2019-12-10 16:19:00 -1000

    Signing up users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :articles
 
+  get "signup", to: 'users#new'
+  post "users", to: 'users#create'
 end
