e4195458454778eec2a0301e01f99b5eb6fd7da6
Lois Talagrand <lois.talagrand@gmail.com>
2019-12-14 07:02:35 -1000

    Unit tests for category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_13_145413) do
+ActiveRecord::Schema.define(version: 2019_12_14_164448) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 2019_12_13_145413) do
     t.integer "user_id"
     t.string "img_url"
     t.string "item_url"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
